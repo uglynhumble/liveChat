952f2c4afdbdf1403d148b2ccb72f4c01897aa75
uglynhumble <platon.luk.98@mail.ru>
2024-09-07 16:47:43 +0300

    feat/backend

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Post } from '@nestjs/common';
+import { PusherService } from './pusher.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  constructor(private pusherService: PusherService) {}
+  @Post('messages')
+  async messages(
+    @Body('username') username: string,
+    @Body('message') message: string,
+  ) {
+    await this.pusherService.trigger('chat', 'message', {
+      username,
+      message,
+    });
+    return [];
   }
 }
---
+++ src/pusher.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import * as Pusher from 'pusher';
+
+@Injectable()
+export class PusherService {
+  pusher: Pusher;
+  constructor() {
+    this.pusher = new Pusher({
+      appId: '1861448',
+      key: '976651470d83ab057dcd',
+      secret: 'a3b9a951f84a2f3bf859',
+      cluster: 'eu',
+      useTLS: true,
+    });
+  }
+  async trigger(channel: string, event: string, data: any) {
+    await this.pusher.trigger(channel, event, data);
+  }
+}
